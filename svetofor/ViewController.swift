293597996b4c68cd8f0e5b723546bfcb9514b19e
Maksim <potapovmv94@gmail.com>
2021-03-23 16:37:57 +0300

    Initial Commit

---
+++ svetofor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  svetofor
+//
+//  Created by Admin1 on 23.03.2021.
+//  Copyright © 2021 potapov_potapov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
