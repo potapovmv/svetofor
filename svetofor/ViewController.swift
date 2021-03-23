f9c513c579e264f01dd9035559257289f8f881be
Maksim <potapovmv94@gmail.com>
2021-03-23 17:08:46 +0300

    code

--- svetofor/ViewController.swift
+++ svetofor/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startButtonOutlet: UIButton!
+
+    @IBOutlet weak var greenSignalView: UIView!
+    @IBOutlet weak var yellowSignalView: UIView!
+    @IBOutlet weak var redSignalView: UIView!
+    override func viewWillLayoutSubviews() {
+        redSignalView.layer.cornerRadius = 60
+        yellowSignalView.layer.cornerRadius = 60
+        greenSignalView.layer.cornerRadius = 60
+       
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        yellowSignalView.alpha = 0.3
+        greenSignalView.alpha = 0.3
+        redSignalView.alpha = 0.3
+        startButtonOutlet.layer.cornerRadius = 10
     }
 
+    @IBAction func startButton(_ sender: UIButton) {
+        startButtonOutlet.setTitle("NEXT", for: .normal)
+        if redSignalView.alpha == 1 {
+            redSignalView.alpha = 0.3
+            yellowSignalView.alpha = 1
+        }else if yellowSignalView.alpha == 1 {
+            yellowSignalView.alpha = 0.3
+            greenSignalView.alpha = 1
+        }else if greenSignalView.alpha == 1 {
+            greenSignalView.alpha = 0.3
+            redSignalView.alpha = 1
+        }else {
+            redSignalView.alpha = 1
+        }
+       
+    }
 
 }
 
